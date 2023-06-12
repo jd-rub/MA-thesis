3e88fa71bb470fba6d254248965782842fe5a8fe
jd-rub <justin.dettmer@rub.de>
2023-06-13 01:43:19 +0200

    add max method

--- evoaudio/pitch_detection.py
+++ evoaudio/pitch_detection.py
@@ -31,6 +31,7 @@ def extract_pitch_probabilities(y, sr=22050, n_bins=88, bins_per_octave=12, fmin
     abs_cqt = np.abs(cqt)
     clipped_cqt = np.clip(abs_cqt, a_min=0, a_max=None)
     cqt_sum = np.sum(clipped_cqt, axis=1)
+    # cqt_sum = np.max(clipped_cqt, axis=1)
     part_idx = np.argpartition(cqt_sum, kth=-k_pitches)[-k_pitches]
     part_val = cqt_sum[part_idx]
     cqt_sum[cqt_sum < (part_val - 0.0001)] = 0
